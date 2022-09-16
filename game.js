67b3f1c2867a329da54ed9d3437f45852a4e0212
Philippovitsch <philipp@stadter.at>
2022-09-16 20:55:07 +0200

    Refactor codebase

--- game.js
+++ game.js
@@ -1,319 +1,337 @@
-const moveUserFunction = (event) => moveUser(event);
+const movePlayerFunction = (event) => movePlayer(event);
 const scoreNode = document.querySelector('#score');
-const gridNode = document.querySelector('.grid');
-
+const boardNode = document.querySelector('.board');
 const gameConfiguration = Object.freeze({
-    ballDiameter: 40,
     boardWidth: 670,
+    ballDiameter: 40,
+    ballRadius: 20,
     boardHeight: 500,
     blockWidth: 100,
     blockHeight: 20,
     gapWidth: 10,
     rowHeight: 30,
     blocksPerRow: 6,
-    xIndexCoordinate: 0,
-    yIndexCoordinate: 1,
-    defaultMaxBlock: 18,
+    IndexOfXCoordinate: 0,
+    IndexOfYCoordinate: 1,
+    defaultMaxBlocks: 18,
+    playerMovement: 25,
     colors: ['#78B07A', '#91B4E1', '#E1CE91', '#D691E1', '#CBE191']
-})
-
+});
 
 let gameIsRunning = false;
-let userPosition = [280, 480];
-let ballPosition = [310, 440];
-let hour = 0;
-let minute = 0;
-let second = 0;
+let playerPosition = [280, 470];
+let ballPosition = [310, 430];
+let ballSpeed = 20;
 let xMovement = -2;
 let yMovement = -2;
+let second = 0;
+let minute = 0;
+let hour = 0;
 let score = 0;
-let ballId;
+
+let blockCoordinates;
 let timerId;
-let ballSpeed = 20;
-
-let blockCoordinates = generateCoordinates();
-let user = placeUser();
-let ball = placeBall();
+let ballId;
+let player;
+let ball;
 
 
+// game initialization
+function initGame() {
+    blockCoordinates = generateCoordinates();
+    player = createPlayer();
+    ball = createBall();
+    document.addEventListener('keydown', movePlayerFunction);
+    createBlocks();
+}
 
 
-function generateCoordinates(maxBlocks = gameConfiguration.defaultMaxBlock) {
-    let blockCoordinates = [];
+// return an array containing the coordinates of all blocks
+function generateCoordinates(maxBlocks = gameConfiguration.defaultMaxBlocks) {
+    let coordinates = [];
     let leftSpace = 0;
     let counter = 0;
     while (counter < maxBlocks) {
         let row = Math.floor(counter / gameConfiguration.blocksPerRow);
-        blockCoordinates.push([leftSpace, gameConfiguration.rowHeight * row]);
-        leftSpace = (counter % gameConfiguration.blocksPerRow) === 5 ? 0 : leftSpace + gameConfiguration.blockWidth + gameConfiguration.gapWidth;
+        coordinates.push([leftSpace, gameConfiguration.rowHeight * row]);
+        leftSpace = (counter % gameConfiguration.blocksPerRow) === gameConfiguration.blocksPerRow - 1 ? 0 : leftSpace + gameConfiguration.blockWidth + gameConfiguration.gapWidth;
         counter++;
     }
-    return blockCoordinates;
+    return coordinates;
 }
 
 
+// pick a random color for the blocks
 function pickRandomColor() {
-
     return gameConfiguration.colors[Math.floor(Math.random() * gameConfiguration.colors.length)];
 }
 
 
-function placeBlocks() {
+// display the blocks on the board
+function createBlocks() {
     for (let block of blockCoordinates) {
         const blockElement = document.createElement('div');
         let color = pickRandomColor();
         blockElement.classList.add('block');
-        blockElement.style.left = block[gameConfiguration.xIndexCoordinate] + 'px';
-        blockElement.style.top = block[gameConfiguration.yIndexCoordinate] + 'px';
         blockElement.style.backgroundColor = color;
-        gridNode.appendChild(blockElement);
+        alignElement(blockElement, block);
+        boardNode.appendChild(blockElement);
     }
 }
 
 
-function placeUser() {
-    const user = document.createElement('div');
-    user.classList.add('user');
-    user.style.left = userPosition[gameConfiguration.xIndexCoordinate] + 'px';
-    user.style.top = userPosition[gameConfiguration.yIndexCoordinate] + 'px';
-    gridNode.appendChild(user);
-    return user;
+// display the player on the board
+function createPlayer() {
+    const playerElement = document.createElement('div');
+    playerElement.classList.add('player');
+    alignElement(playerElement, playerPosition);
+    boardNode.appendChild(playerElement);
+    return playerElement;
 }
 
 
-function placeBall() {
-    const ball = document.createElement('div');
-    ball.style.left = ballPosition[gameConfiguration.xIndexCoordinate] + 'px';
-    ball.style.top = ballPosition[gameConfiguration.yIndexCoordinate] + 'px';
-    ball.classList.add('ball');
-    gridNode.appendChild(ball);
-    return ball;
+// display the ball on the board
+function createBall() {
+    const ballElement = document.createElement('div');
+    ballElement.classList.add('ball');
+    ballElement.textContent = '🍩';
+    alignElement(ballElement, ballPosition);
+    boardNode.appendChild(ballElement);
+    return ballElement;
 }
 
-// TODO : call it in the time func
+
+// increase the ball speed at a certain score
 function increaseBallSpeed() {
     switch (score) {
         case 5:
             ballSpeed = 17;
-            clearInterval(ballId);
-            ballId = setInterval(moveBall, ballSpeed);
+            ballId = changeIntervalDelay(ballId)
             break;
         case 10:
             ballSpeed = 14;
-            clearInterval(ballId);
-            ballId = setInterval(moveBall, ballSpeed);
+            ballId = changeIntervalDelay(ballId)
             break;
         case 15:
             ballSpeed = 11;
-            clearInterval(ballId);
-            ballId = setInterval(moveBall, ballSpeed);
+            ballId = changeIntervalDelay(ballId)
             break;
         case 20:
             ballSpeed = 8;
-            clearInterval(ballId);
-            ballId = setInterval(moveBall, ballSpeed);
+            ballId = changeIntervalDelay(ballId)
             break;
     }
 }
 
 
-function moveUser(event) {
+// change the delay of an interval
+function changeIntervalDelay(intervalId) {
+    clearInterval(intervalId);
+    return setInterval(moveBall, ballSpeed);
+}
+
+
+// set the intervals on the first keypress
+function startGame() {
     if (gameIsRunning === false) {
-        timerId = setInterval(() => { timer(); }, 1000);
+        timerId = setInterval(timer, 1000);
         ballId = setInterval(moveBall, ballSpeed);
         gameIsRunning = true;
     }
+}
 
+
+// move the player by pressing a key
+function movePlayer(event) {
+    startGame();
     switch (event.key) {
+        case 'a':
+        case 'A':
         case 'ArrowLeft':
-            if (userPosition[gameConfiguration.xIndexCoordinate] > 5) {
-                userPosition[gameConfiguration.xIndexCoordinate] -= 25;
-                drawUser();
+            if (playerPosition[gameConfiguration.IndexOfXCoordinate] > 5) {
+                playerPosition[gameConfiguration.IndexOfXCoordinate] -= gameConfiguration.playerMovement;
+                alignElement(player, playerPosition);
             }
-            break
+            break;
+        case 'd':
+        case 'D':
         case 'ArrowRight':
-            if (userPosition[gameConfiguration.xIndexCoordinate] < (gameConfiguration.boardWidth - gameConfiguration.blockWidth - 20)) {
-                userPosition[gameConfiguration.xIndexCoordinate] += 25;
-                drawUser();
+            if (playerPosition[gameConfiguration.IndexOfXCoordinate] < (gameConfiguration.boardWidth - gameConfiguration.blockWidth - gameConfiguration.playerMovement)) {
+                playerPosition[gameConfiguration.IndexOfXCoordinate] += gameConfiguration.playerMovement;
+                alignElement(player, playerPosition);
             }
-            break
+            break;
     }
 }
 
 
-function drawUser() {
-    user.style.left = userPosition[gameConfiguration.xIndexCoordinate] + 'px';
-    user.style.top = userPosition[gameConfiguration.yIndexCoordinate] + 'px';
+// align an element on the board
+function alignElement(element, elementPosition) {
+    element.style.left = elementPosition[gameConfiguration.IndexOfXCoordinate] + 'px';
+    element.style.top = elementPosition[gameConfiguration.IndexOfYCoordinate] + 'px';
 }
 
 
-function drawBall() {
-    ball.style.left = ballPosition[gameConfiguration.xIndexCoordinate] + 'px';
-    ball.style.top = ballPosition[gameConfiguration.yIndexCoordinate] + 'px';
+// move the ball on the board 
+function moveBall(){
+    ballPosition[gameConfiguration.IndexOfXCoordinate] += xMovement;
+    ballPosition[gameConfiguration.IndexOfYCoordinate] += yMovement;
+    alignElement(ball, ballPosition);
+    checkForCollisions();
 }
 
 
-function moveBall() {
-    ballPosition[gameConfiguration.xIndexCoordinate] += xMovement;
-    ballPosition[gameConfiguration.yIndexCoordinate] += yMovement;
-    drawBall();
-    checkTheCollisions();
-}
-
-
-function checkTheCollisions() {
-
-
-    //check for block collision
-    for (let index = 0; index < blockCoordinates.length; index++) {
-        let bottomLeft = [blockCoordinates[index][gameConfiguration.xIndexCoordinate], blockCoordinates[index][gameConfiguration.yIndexCoordinate] + gameConfiguration.blockHeight];
-        let bottomRight = [blockCoordinates[index][gameConfiguration.xIndexCoordinate] + gameConfiguration.blockWidth, blockCoordinates[index][gameConfiguration.yIndexCoordinate] + gameConfiguration.blockHeight];
-        let topLeft = [blockCoordinates[index][gameConfiguration.xIndexCoordinate], blockCoordinates[index][gameConfiguration.yIndexCoordinate]];
-        if (
-            (ballPosition[gameConfiguration.xIndexCoordinate] > bottomLeft[gameConfiguration.xIndexCoordinate] && ballPosition[gameConfiguration.xIndexCoordinate] < bottomRight[gameConfiguration.xIndexCoordinate]) &&
-            ((ballPosition[gameConfiguration.yIndexCoordinate] - gameConfiguration.ballDiameter / 2) < bottomLeft[gameConfiguration.yIndexCoordinate] && ballPosition[gameConfiguration.yIndexCoordinate] > topLeft[gameConfiguration.yIndexCoordinate])
-        ) {
-            const allBlocks = addPlayerScore(index);
-
-            // winning condition
-            checkPlayerWin(allBlocks);
+// check if the ball hits an element
+function checkForCollisions(){
+    for (let index = 0; index < blockCoordinates.length ; index++ ) {
+        if (hitBlock(index)) {
+            const allBlocks = Array.from(document.querySelectorAll('.block'));
+            allBlocks[index].classList.remove('block');
+            blockCoordinates.splice(index, 1);
+            score++;
+            scoreNode.textContent = String('Score: ' + score);
+            checkIfPlayerWins(allBlocks);
+            changeDirection();
+            increaseBallSpeed();
         }
     }
 
-    // check for wall hits
-    checkHitToWall();
-
-    //check for user collision
-    checkHitUser();
-
-    // game over
-    checkGameOver();
+    if (hitWall()) changeDirection();
+    if (hitPlayer()) changeDirection(randomAngle = true);
+    checkIfPlayerLooses();
 }
 
 
-function checkHitUser() {
-    if ((ballPosition[gameConfiguration.xIndexCoordinate] > userPosition[gameConfiguration.xIndexCoordinate] && ballPosition[gameConfiguration.xIndexCoordinate] < userPosition[gameConfiguration.xIndexCoordinate] + gameConfiguration.blockWidth) &&
-        (ballPosition[gameConfiguration.yIndexCoordinate] < userPosition[gameConfiguration.yIndexCoordinate] && ballPosition[gameConfiguration.yIndexCoordinate] > userPosition[gameConfiguration.yIndexCoordinate] - gameConfiguration.ballDiameter)) {
-        changeDirection(true);
-    }
+// check if the ball hits a block
+function hitBlock(index) {
+    let ballXPosition = ballPosition[gameConfiguration.IndexOfXCoordinate] + gameConfiguration.ballRadius;
+    let ballYPosition = ballPosition[gameConfiguration.IndexOfYCoordinate];
+    let topLeftCornerYPosition = blockCoordinates[index][gameConfiguration.IndexOfYCoordinate];
+    let bottomLeftCornerXPosition = blockCoordinates[index][gameConfiguration.IndexOfXCoordinate];
+    let bottomLeftCornerYPosition = blockCoordinates[index][gameConfiguration.IndexOfYCoordinate] + gameConfiguration.blockHeight;
+    let bottomRightCornerXPosition = blockCoordinates[index][gameConfiguration.IndexOfXCoordinate] + gameConfiguration.blockWidth;
+    
+    return (ballXPosition > bottomLeftCornerXPosition && ballXPosition < bottomRightCornerXPosition) &&
+           (ballYPosition > topLeftCornerYPosition && ballYPosition < bottomLeftCornerYPosition);
 }
 
-function checkHitToWall() {
-    if (ballPosition[gameConfiguration.xIndexCoordinate] >= (gameConfiguration.boardWidth - gameConfiguration.ballDiameter) || ballPosition[gameConfiguration.xIndexCoordinate] <= 0 || ballPosition[gameConfiguration.yIndexCoordinate] < 0) {
-        changeDirection();
-    }
+
+// check if the ball hits a wall
+function hitWall() {
+    let ballXPosition = ballPosition[gameConfiguration.IndexOfXCoordinate];
+    let ballYPosition = ballPosition[gameConfiguration.IndexOfYCoordinate];
+    let rightWall = (gameConfiguration.boardWidth - gameConfiguration.ballDiameter);
+    let leftWall = 0;
+    let topWall = 0;
+
+    return ballXPosition >= rightWall || ballXPosition <= leftWall || ballYPosition < topWall;
 }
 
-function checkGameOver() {
-    if (ballPosition[1] >= gameConfiguration.boardHeight - gameConfiguration.ballDiameter) {
-        clearInterval(ballId);
-        clearInterval(timerId);
-        scoreNode.innerHTML = 'You lost!';
-        document.removeEventListener('keydown', moveUserFunction);
-    }
+
+// check if the ball hits the player
+function hitPlayer() {
+    let ballXPosition = ballPosition[gameConfiguration.IndexOfXCoordinate] + gameConfiguration.ballRadius;
+    let ballYPosition = ballPosition[gameConfiguration.IndexOfYCoordinate] + gameConfiguration.ballDiameter;
+    let playerXPosition = playerPosition[gameConfiguration.IndexOfXCoordinate];
+    let playerYPosition = playerPosition[gameConfiguration.IndexOfYCoordinate];
+
+    return (ballXPosition > playerXPosition && ballXPosition < playerXPosition + gameConfiguration.blockWidth) &&
+           (ballYPosition > playerYPosition && ballYPosition < playerYPosition + gameConfiguration.blockHeight);
 }
 
-function checkPlayerWin(allBlocks) {
+
+// the player wins (no more blocks left)
+function checkIfPlayerWins(allBlocks) {
     if (allBlocks.length <= 1) {
         clearInterval(ballId);
         clearInterval(timerId);
-        scoreNode.innerHTML = 'You won!';
-        document.removeEventListener('keydown', moveUserFunction);
+        displayMessage('You won!');
+        document.removeEventListener('keydown', movePlayerFunction);
     }
 }
 
-function addPlayerScore(index) {
-    const allBlocks = Array.from(document.querySelectorAll('.block'));
-    allBlocks[index].classList.remove('block');
-    blockCoordinates.splice(index, 1);
-    changeDirection();
-    score++;
-    increaseBallSpeed();
-    scoreNode.innerHTML = String('Score: ' + score);
-    return allBlocks;
+
+// game over (the ball reaches the bottom wall)
+function checkIfPlayerLooses() {
+    let ballYPosition = ballPosition[gameConfiguration.IndexOfYCoordinate] + gameConfiguration.ballDiameter;
+    if (ballYPosition >= gameConfiguration.boardHeight) {
+        clearInterval(ballId);
+        clearInterval(timerId);
+        displayMessage('You lost!');
+        document.removeEventListener('keydown', movePlayerFunction);
+    }
 }
 
-function changeDirection(random = false) {
+
+// display a message on the board
+function displayMessage(text) {
+    const message = document.createElement('div');
+    message.classList.add('text-message');
+    message.textContent = text;
+    boardNode.appendChild(message);
+}
+
+
+// change the direction of the ball
+function changeDirection(randomAngle = false) {
     if (xMovement > 0 && yMovement < 0) {
         yMovement = 2;
-
-    }
-    else if (xMovement > 0 && yMovement > 0) {
-        if (random === true) {
-            xMovement = generateRandomNumber() * -1;
-        } else {
-            xMovement *= -1;
-        }
-
-    }
-    else if (xMovement < 0 && yMovement > 0) {
+    } else if (xMovement > 0 && yMovement > 0) {
+        xMovement = (randomAngle === true) ? generateRandomNumber() * -1 : xMovement * -1;
+    } else if (xMovement < 0 && yMovement > 0) {
         yMovement = -2;
-
-    }
-    else {
-        if (random === true) {
-            xMovement = generateRandomNumber();
-        } else {
-            xMovement *= -1;
-        }
-
+    } else {
+        xMovement = (randomAngle === true) ? generateRandomNumber() : xMovement * -1;
     }
 }
 
 
+// generate a random number (for a random ball angle)
 function generateRandomNumber() {
-    return Math.ceil(Math.random() * 6);
+    return Math.ceil(Math.random() * gameConfiguration.blocksPerRow);
 }
 
 
-const addOneSecond = () => second++;
+// convert the time and display it on the board
 function timer() {
+    const addOneSecond = () => second++;
+    const addOneHour = () => {minute = 0; hour++;}
+    const addOneMinute = () => {second = 0; minute++;}
 
     addOneSecond();
-    second === 60 && addOneMinute();
-    minute === 60 && addOneHour();
+    if (second === 60) addOneMinute();
+    if (minute === 60) addOneHour();
 
-    document.getElementById('hour').innerText = returnData(hour);
-    document.getElementById('minute').innerText = returnData(minute);
-    document.getElementById('second').innerText = returnData(second);
+    document.getElementById('hour').textContent = formatTime(hour);
+    document.getElementById('minute').textContent = formatTime(minute);
+    document.getElementById('second').textContent = formatTime(second);
 
-    if (second % 30 === 0) {
-        generateNewLine();
-    }
+    if (second % 30 === 0) generateNewRow();
 }
 
 
-function addOneHour() {
-    minute = 0;
-    hour++;
-}
-
-function addOneMinute() {
-    second = 0;
-    minute++;
-}
-
-function returnData(input) {
-    let singleCharDigits = 9;
+// add leading zero to single digit (0-9)
+function formatTime(input) {
+    const singleCharDigits = 9;
     return input > singleCharDigits ? input : `0${input}`;
 }
 
 
-function generateNewLine() {
-    let blocks = Array.from(gridNode.childNodes);
+// generate a new row of blocks
+function generateNewRow() {
+    const blocks = Array.from(boardNode.childNodes);
     for (let block of blocks) {
-        if (block.classList != 'ball' && block.classList != 'user') {
+        if (block.className != 'ball' && block.className != 'player') {
             block.remove();
-        }
+        } 
     }
     for (let coordinate of blockCoordinates) {
-        coordinate[gameConfiguration.yIndexCoordinate] += gameConfiguration.rowHeight;
+        coordinate[gameConfiguration.IndexOfYCoordinate] += gameConfiguration.rowHeight;
     }
-    let newLine = generateCoordinates(gameConfiguration.blocksPerRow);
-    blockCoordinates = newLine.concat(blockCoordinates);
-    placeBlocks();
+    let newRow = generateCoordinates(gameConfiguration.blocksPerRow);
+    blockCoordinates = newRow.concat(blockCoordinates);
+    createBlocks();
 }
 
 
-document.addEventListener('keydown', moveUserFunction);
-placeBlocks();
+initGame()
